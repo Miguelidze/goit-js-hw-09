44d7e6bc78e77e78884516b9f75ab85bd0229d87
Miguelidze <migueldeorpesa@gmail.com>
2021-12-10 19:44:43 +0100

    Update 01-color-switcher.js
    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-set]');
+
+let timerId = null;
+
+stopBtn.setAttribute('disabled', true);
+
+console.log();
+setTimeout(() => {
+    console.log()
+}, 1000)
